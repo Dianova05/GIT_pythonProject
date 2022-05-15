fb8a04920ec13d40e7b05012c646ae5cde40caf0
DianovaAA <Dianova05@gmail.com>
2022-05-16 01:26:35 +0300

    Практическое задание №5

---
+++  Homework_Dianova_AA/homework_05/number_05.py
@@ -0,0 +1,18 @@
+# This Python file uses the following encoding: utf-8
+import os, sys
+# 5. Создать (программно) текстовый файл, записать в него программно набор чисел,
+# разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
+# This Python file uses the following encoding: utf-8
+def my_file():
+    try:
+        with open('test_05.txt', 'w+') as file_obj:
+            line = input('Введите цифры через пробел \n')
+            file_obj.writelines(line)
+            my_numb = line.split()
+
+            print(sum(map(int, my_numb)))
+    except IOError:
+        print('Ошибка в файле')
+    except ValueError:
+        print('Неправильно набран номер. Ошибка ввода-вывода')
+my_file()
